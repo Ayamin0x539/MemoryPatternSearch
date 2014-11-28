28d5ebaec0d769d826b4a6209c7aea91cfa4cbd2
Ayamin0x539 <ayamin1337@yahoo.com>
2014-11-28 08:59:07 -0500

    Fixed some comments.

--- MemoryPatternSearch/dllmain.cpp
+++ MemoryPatternSearch/dllmain.cpp
@@ -15,14 +15,14 @@ as long as you know the byte array of the structure that contains such informati
 using namespace std;
 
 // Actual functions' bodies are defined at the end of this file.
-bool isPattern(const BYTE* pData, const BYTE* bMask, const char* szMask);
-DWORD patternSearch(DWORD dwAddress, DWORD dwLen, BYTE *bMask, char * szMask);
+bool isPattern(const BYTE* pData, const BYTE* byteArray, const char* szMask);
+DWORD patternSearch(DWORD dwAddress, DWORD dwLen, BYTE* byteArray, char* szMask);
 
 // Change accordingly:
 #define START 0x30000000 // put start address here
 #define DELTA 0x00200000 // put the length of search here
 #define PATTERN "\x44\x34\x24\x01" // byte array pattern here in this syntax. Can be as long as you want
-#define MASK "xxxx" // the mask - make sure there are as many characters in this string as there are bytes in the pattern.
+#define MASK "xx??" // the mask - make sure there are as many characters in this string as there are bytes in the pattern.
 // --> format for the mask: x for what you want to be included in the search, ? for wildcards.
 //   --> see the algorithm description for more details down below.
 
@@ -89,7 +89,7 @@ void loop() {
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
 	if (ul_reason_for_call == DLL_PROCESS_ATTACH) { // we will be injecting the dll into the process
 		MessageBoxA(NULL, "Made by Ayamin.", "Successful injection!", MB_OK); // messagebox showing credits
-		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&loop, 0, 0, 0); // multi-thread the loop function by passing in its address
+		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&loop, 0, 0, 0); // multi-thread the loop function by passing in its address to WinAPI multithreading function
 	}
 	return TRUE;
 }
@@ -101,39 +101,41 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 */
 
 // isPattern:
-// takes an address in memory, a byte mask (the byte array) to find, and a string of tokens denoting which bytes to ignore.
-// searches through the byte array to find a match at a ***particular point in memory***
+// takes an address in memory, a byte array to find, and a string of tokens denoting which bytes to ignore.
+// searches through the byte array to find a match at that ***particular point in memory***
+// goes through szMask to see which bytes to "ignore".
 
-bool isPattern(const BYTE* pData, const BYTE* bMask, const char* szMask)
+bool isPattern(const BYTE* pData, const BYTE* byteArray, const char* szMask)
 {
-	for (; *szMask; ++szMask, ++pData, ++bMask){
-		if (*szMask == 'x' && *pData != *bMask) {
+	for (; *szMask; ++szMask, ++pData, ++byteArray) { // increment through the mask, data, and byteArray. They are all pointers so we can do this.
+		if (*szMask == 'x' && *pData != *byteArray) {
 			// this guard is true only when we reach a byte we want to evaluate (*szMask == 'x')
 			// AND if at this point, the data doesn't match what we are looking for (the data being the instructions at the memory address we are currently at)
 			return false; // will only be reached when a non-match occurs (aka, if we match, we keep going)
 		}
 	}
-	return (*szMask) == NULL; // will always return true when all bytes in the byte-array bMask have been exhausted - this will be at the same time that all tokens in szMask have been exhausted
-	// i.e., we have compared every single byte in the byte array and it has all matched in the memory.
+	return (*szMask) == NULL; // will always return true when all bytes in the byte-array byteArray have been exhausted - this will be at the same time that all tokens in szMask have been exhausted
+	// i.e., we have compared every single byte in the byte array and it has all matched in the memory. then szMask will point to nothing (hence, *szMask == NULL)
 }
 
 /* takes a start address dwAddress and a length dwLen //
-runs a search from the start address for a pattern of bytes, the byte array bMask //
+runs a search from the start address for a pattern of bytes, the byte array byteArray //
 we have a mask that filters out variables (denoted by ?'s) denoted as "wildcards" //
 such "wildcards" are not counted in the byte pattern
-(i.e., if we have bMask (BYTE*)("\x40\x23\xB9\x1F") and are using the string mask "xx??",
+(i.e., if we have byteArray (BYTE*)("\x40\x23\xB9\x1F") and are using the string mask "xx??",
 then "\x40\x23\x9F\x26" will still count as a match, because the last two bytes are excluded and considered anomalies)
 -- in most games, these will be the bytes that change throughout updates. other bytes will still be consistent in the function we are looking for
 
 
-=== Complexity remark: operates in O(nm) time in worst case; n is the length of the byte-array (szMask), m is the length of memory we are searching (dwLen) ===
+=== Complexity remark: operates in O(nm) time in worst case; n is the length of the byte-array (byteArray), m is the length of memory we are searching (dwLen) ===
+--- proof: for each memory address (there are m of them), we would make n comparisons (each byte
 
 */
 
-DWORD patternSearch(DWORD dwAddress, DWORD dwLen, BYTE *bMask, char* szMask)
+DWORD patternSearch(DWORD dwAddress, DWORD dwLen, BYTE* byteArray, char* szMask)
 {
 	for (DWORD i = 0; i < dwLen; i++) {
-		if (isPattern((BYTE*)(dwAddress + i), bMask, szMask)) { // isPattern determines if the instructions that start at a single address matches our array of bytes'
+		if (isPattern((BYTE*)(dwAddress + i), byteArray, szMask)) { // isPattern determines if the instructions that start at a single address matches our array of bytes'
 			return (DWORD)(dwAddress + i); // if so, return that address - we have found it!
 		}
 	}
